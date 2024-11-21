9e9a1a10cfe1a5e884ab9af3ea2f3f6103f53b60
rob <robwoo8426@gmail.com>
2024-11-21 21:08:25 +0200

    Build slurping for node + basic parse

--- src/CSV_PNL/core.cljs
+++ src/CSV_PNL/core.cljs
@@ -1,6 +1,35 @@
-(ns CSV-PNL.core)
+(ns CSV-PNL.core
+  (:require
+   [cljs.nodejs :as nodejs] 
+   [clojure.string :as string]))
 
 (println "Starting Point")
 
 (defn ^export create-pnl [csv] 
   (println "CSV Uploaded"))
+
+
+(defn parse-line [line]
+  (string/split line #","))
+
+(defn parse-csv [csv-string]
+  (let [lines (string/split-lines csv-string)
+        headers (map keyword (parse-line (first lines)))
+        rows (map parse-line (rest lines))]
+    (map #(zipmap headers %) rows)))
+
+
+(def fs (nodejs/require "fs"))
+
+(defn node-slurp [file-path]
+  (let [content (.toString (.readFileSync fs file-path))]
+    content
+    ))
+
+
+
+(comment
+  (parse-csv
+   (node-slurp "resources/transaction_data.csv"))
+  
+  )
