43e4edc175d4af4096ee40eb0c56ad643ca9dc6a
Rob <robwoo8426@gmail.com>
2024-11-25 13:36:07 +0200

    Remove obsolete create-pnl-test from core_test.cljs

--- test/CSV_PNL/core_test.cljs
+++ test/CSV_PNL/core_test.cljs
@@ -3,10 +3,6 @@
    [cljs.test :refer-macros [deftest is run-all-tests]]
    [CSV-PNL.core :refer [createpnl]]))
 
-(deftest create-pnl-test
-  (is (= (createpnl "resources/transaction_data.csv")
-         {:income 100 :expense 50 :net-profit 50 :transactions []})))
-
 (comment
 
   (run-all-tests #"CSV-PNL.+"))
